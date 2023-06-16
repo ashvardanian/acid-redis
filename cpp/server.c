68886d95f93e7f2ff55924927ca6d1a645395934
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2023-06-16 16:13:38 +0400

    Refactor: server.c

--- cpp/server.c
+++ cpp/server.c
@@ -9,11 +9,10 @@
 static ustore_database_t db;
 static ustore_arena_t arena;
 
-static void set(ucall_call_t call)
-{
-    int64_t *key_ptr;
-    ucall_str_t value_ptr;
-    size_t value_len;
+static void set(ucall_call_t call) {
+    int64_t* key_ptr = NULL;
+    ucall_str_t value_ptr = NULL;
+    size_t value_len = 0;
 
     bool key_found = ucall_param_named_i64(call, "key", 0, key_ptr);
     bool value_found = ucall_param_named_str(call, "value", 5, &value_ptr, &value_len);
@@ -26,25 +25,24 @@ static void set(ucall_call_t call)
     write.error = &error;
     write.arena = &arena;
     write.tasks_count = 1;
-    write.keys = (ustore_key_t *)(key_ptr);
-    write.lengths = (ustore_length_t *)(&value_len);
-    write.values = (ustore_bytes_cptr_t *)(&value_ptr);
+    write.keys = (ustore_key_t*)(key_ptr);
+    write.lengths = (ustore_length_t*)(&value_len);
+    write.values = (ustore_bytes_cptr_t*)(&value_ptr);
 
     ustore_write(&write);
-    if (error==NULL)
+    if (error == NULL)
         return ucall_call_reply_content(call, "OK", 2);
     return ucall_call_reply_error(call, -32603, "Set Failure.", 12);
 }
 
-static void get(ucall_call_t call)
-{
-    int64_t *key_ptr;
+static void get(ucall_call_t call) {
+    int64_t* key_ptr = NULL;
     bool key_found = ucall_param_named_i64(call, "key", 0, key_ptr);
     if (!key_found)
         return ucall_call_reply_error_invalid_params(call);
 
     ustore_error_t error = NULL;
-    ustore_length_t *found_lengths;
+    ustore_length_t* found_lengths;
     ustore_bytes_ptr_t found_values;
 
     ustore_read_t read;
@@ -52,26 +50,24 @@ static void get(ucall_call_t call)
     read.error = &error;
     read.arena = &arena;
     read.tasks_count = 1;
-    read.keys = (ustore_key_t *)(key_ptr);
+    read.keys = (ustore_key_t*)(key_ptr);
     read.lengths = &found_lengths;
     read.values = &found_values;
 
     ustore_read(&read);
-    if (error==NULL)
+    if (error == NULL)
         return ucall_call_reply_content(call, (ucall_str_t)(*found_values), *found_lengths);
     return ucall_call_reply_error(call, -32603, "Get Failure.", 12);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
 
     ustore_error_t error = NULL;
     ustore_database_init_t database_init;
     database_init.db = &db;
     database_init.error = &error;
     ustore_database_init(&database_init);
-    if (error!=NULL)
-    {
+    if (error != NULL) {
         printf("Failed To Open DataBase!\n");
         return -1;
     }
@@ -80,8 +76,7 @@ int main(int argc, char **argv)
     ucall_config_t config;
     config.port = 6378;
     ucall_init(&config, &server);
-    if (!server)
-    {
+    if (!server) {
         printf("Failed To Initialize Server!\n");
         return -1;
     }
