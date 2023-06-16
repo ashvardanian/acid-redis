53f5de8971ad27e8285ab0c6f450993e244d3df8
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2023-06-16 16:19:18 +0400

    Refactor: server.c

--- cpp/server.c
+++ cpp/server.c
@@ -72,9 +72,9 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    ucall_server_t server;
     ucall_config_t config;
     config.port = 6378;
+    ucall_server_t server = NULL;
     ucall_init(&config, &server);
     if (!server) {
         printf("Failed To Initialize Server!\n");
